a910b0ed34b4ece6660c6cb39d4dca0c40f76986
Stefan Heimes <stefan_heimes@hotmail.com>
2014-12-18 11:45:24 +0100

    Cleanup the DCA files.

--- system/modules/x-seo/dca/tl_page.php
+++ system/modules/x-seo/dca/tl_page.php
@@ -3,22 +3,34 @@
 /**
  * Contao Open Source CMS
  *
- * @copyright  MEN AT WORK 2013 
+ * @copyright  MEN AT WORK 2013
  * @package    extendedSEO
  * @license    GNU/LGPL
  * @filesource
  */
 
-$this->loadLanguageFile('tl_article');
-$this->loadLanguageFile('tl_settings');
+/**
+ * PLoad some language files.
+ */
+try
+{
+	\Controller::loadLanguageFile('tl_article');
+	\Controller::loadLanguageFile('tl_settings');
+}
+catch (Exception $e)
+{
+	// Die silently.
+}
 
 /**
  * Palettes
  */
+
+// Add the description and keywords.
 foreach ($GLOBALS['TL_DCA']['tl_page']['palettes'] as $keyPalette => $valuePalette)
 {
 	// Skip if we have a array or the palettes for subselections
-	if (is_array($valuePalette) || $keyPalette == "__selector__")
+	if (is_array($valuePalette) || $keyPalette == '__selector__')
 	{
 		continue;
 	}
@@ -29,51 +41,62 @@ foreach ($GLOBALS['TL_DCA']['tl_page']['palettes'] as $keyPalette => $valuePalet
 	// Search for "{meta_legend}" and insert the fields
 	foreach ($arrEntries as $keyEntry => $valueEntry)
 	{
-		if (stripos($valueEntry, "{meta_legend}") !== false)
+		if (stripos($valueEntry, '{meta_legend}') !== false)
 		{
-			$arrEntry = trimsplit(",", $valueEntry);
+			$arrEntry = trimsplit(',', $valueEntry);
 
-			if (($mixSearch = array_search("description", $arrEntry)) !== FALSE)
+			// First remove the current fields.
+			if (($mixSearch = array_search('description', $arrEntry)) !== false)
 			{
-				unset($arrEntry[$mixSearch]);
+				unset($arrEntry[ $mixSearch ]);
 			}
 
-			if (($mixSearch = array_search("description", $arrEntry)) !== FALSE)
+			if (($mixSearch = array_search('keywords', $arrEntry)) !== false)
 			{
-				unset($arrEntry[$mixSearch]);
+				unset($arrEntry[ $mixSearch ]);
 			}
 
-			$arrEntry = array_merge(array_slice($arrEntry, 0, 2), array("description", "keywords"), array_slice($arrEntry, 2, count($arrEntry) - 2));
+			// Than add a fields.
+			$arrEntry = array_merge(array_slice($arrEntry, 0, 2), array('description', 'keywords'), array_slice($arrEntry, 2, count($arrEntry) - 2));
 
-			$valueEntry = implode(",", $arrEntry);
-			$arrEntries[$keyEntry] = $valueEntry;
+			$valueEntry              = implode(",", $arrEntry);
+			$arrEntries[ $keyEntry ] = $valueEntry;
 		}
 	}
 
 	// Write new entry back in the palette
-	$GLOBALS['TL_DCA']['tl_page']['palettes'][$keyPalette] = implode(";", $arrEntries);
+	$GLOBALS['TL_DCA']['tl_page']['palettes'][ $keyPalette ] = implode(";", $arrEntries);
 }
 
+// Add the xseo_rootTitle field.
 foreach ($GLOBALS['TL_DCA']['tl_page']['palettes'] as $key => $row)
 {
-	if ($key == '__selector__' || $key == 'root') continue;
-	if (!stristr($row, 'pageTitle')) continue;
-	$GLOBALS['TL_DCA']['tl_page']['palettes'][$key] = str_replace('pageTitle', 'pageTitle,xseo_rootTitle', $GLOBALS['TL_DCA']['tl_page']['palettes'][$key]);
+	if ($key == '__selector__' || $key == 'root')
+	{
+		continue;
+	}
+	elseif (!stristr($row, 'pageTitle'))
+	{
+		continue;
+	}
+	else
+	{
+		$GLOBALS['TL_DCA']['tl_page']['palettes'][ $key ] = str_replace('pageTitle', 'pageTitle,xseo_rootTitle', $GLOBALS['TL_DCA']['tl_page']['palettes'][ $key ]);
+	}
 }
 
 /**
  * Fields
  */
 $GLOBALS['TL_DCA']['tl_page']['fields']['keywords'] = array(
-	'label'          => &$GLOBALS['TL_LANG']['tl_article']['keywords'],
-	'exclude'        => true,
-	'inputType'      => 'textarea',
-	'eval'           => array('style' => 'height:60px;')
+	'label'     => &$GLOBALS['TL_LANG']['tl_article']['keywords'],
+	'exclude'   => true,
+	'inputType' => 'textarea'
 );
 
 $GLOBALS['TL_DCA']['tl_page']['fields']['xseo_rootTitle'] = array(
-	'label'          => &$GLOBALS['TL_LANG']['tl_settings']['websiteTitle'],
-	'exclude'        => true,
-	'inputType'      => 'text',
-	'eval'           => array('tl_class' => 'long clr')
+	'label'     => &$GLOBALS['TL_LANG']['tl_settings']['websiteTitle'],
+	'exclude'   => true,
+	'inputType' => 'text',
+	'eval'      => array('tl_class' => 'long clr')
 );
